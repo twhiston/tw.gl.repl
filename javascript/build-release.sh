04223dce40f5848972eff9e9f0e4a02de83e44b2
TomW <tom.whiston@gmail.com>
2023-07-12 23:14:36 +0100

    ci: remove cruft from release

--- javascript/build-release.sh
+++ javascript/build-release.sh
@@ -10,7 +10,7 @@ rm -rf $PKGROOT
 mkdir -p $PKGPATH
 rsync -av  ./../ $PKGPATH --exclude $PKGROOT
 rm -rf $PKGPATH/.git $PKGPATH/.dist $PKGPATH/.github $PKGPATH/.vscode $PKGPATH/javascript $PKGPATH/.gitignore
-rm -rf $PKGPATH/examples/livecode-max/src $PKGPATH/examples/livecode-max/node_modules $PKGPATH/examples/livecode-max/package* $PKGPATH/examples/livecode-max/tsconfig.json
+rm -rf $PKGPATH/lefthook.yml $PKGPATH/code-coverage-results.md $PKGPATH/commitlint.config.js  $PKGPATH/examples/livecode-max/src $PKGPATH/examples/livecode-max/node_modules $PKGPATH/examples/livecode-max/package* $PKGPATH/examples/livecode-max/tsconfig.json
 mkdir $PKGPATH/javascript
 rsync -av ./dist/* $PKGPATH/javascript
 cd $PKGROOT; zip -r $PKGNAME.zip ./$PKGNAME
